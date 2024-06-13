79309e6fe74c8082b5fa89a41e34726ba586e8f1
Raunak Gupta <116273178+11raunakrg@users.noreply.github.com>
2024-06-13 12:15:28 +0000

    date and time

---
+++ 01_basics/07_date_and_time.js
@@ -0,0 +1,17 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(1915, 1, 35);
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
