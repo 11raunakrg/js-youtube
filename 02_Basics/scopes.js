17739fd1fee03b11ce492ab01f63415ed5afa241
Raunak Gupta <116273178+11raunakrg@users.noreply.github.com>
2024-06-15 06:39:26 +0000

    scopes

--- 02_Basics/scopes.js
+++ 02_Basics/scopes.js
@@ -4,12 +4,49 @@ let a = 300;
     let a = 10;  ///local scope
     const b = 20; ///global scope
 //    var c = 25;  ///global scope  avoid using "var".....
-    console.log(`value of a is ${a}`);
+    // console.log(`value of a is ${a}`);
    
  }
 
-console.log(`value of a is ${a}`);
+// console.log(`value of a is ${a}`);
 // console.log(b);
 // console.log(c);
+
+function one(username) {
+    console.log(`Name is ${username}`);
+
+    function two() {
+        const website = "youtube";
+        console.log(website);
+        
+    }
+    two();
+
+    // console.log(website);    ///////give error
+}
+// two();
+// one("Raunakk");
+
+if(true) {
+    const username = "Raunakkk";
+    if(username === "Raunakkk") {
+        const website = "  youtube";
+        // console.log(username  + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+/////////+++++++++++++++++++++++++++++ Important concept ++++++++++++++++++++++++/////////////////////////
+console.log(addOne(5));
+
+function addOne(num) {
+    return num + 1;
+}
+
+
+console.log(addTwo(9));         //////////gives error(called as hoisting...)////////
+const addTwo = function addTwo(num) {
+    return num + 2;
+}
 
 
