2cdef2e0e40487ed20e4b9e10af7c2f4cc26e042
Raunak Gupta <116273178+11raunakrg@users.noreply.github.com>
2024-06-16 07:28:50 +0000

    for loop

---
+++ control.js/loops.js
@@ -0,0 +1,14 @@
+/// for loop
+
+for (let index = 1; index <= 10; ++index) {
+    const element = index;
+    console.log(element);
+    
+}
+
+for(let i = 1;i <=10;i++) {
+    // console.log(`${357} * ${i} = ${357 * i}`);
+}
+
+///// break and continue
+
