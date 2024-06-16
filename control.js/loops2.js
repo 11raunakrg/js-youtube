e3fd42f19c822002deb49f1ed42349e231a34b8a
Raunak Gupta <116273178+11raunakrg@users.noreply.github.com>
2024-06-16 07:45:11 +0000

    while loop

---
+++ control.js/loops2.js
@@ -0,0 +1,14 @@
+let index = 0;
+
+while(index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index += 2;
+
+}
+
+
+let index2 = 1;
+while(index2 <= 10) {
+    console.log(`${27} * ${index2} is ${27 * index2}`);
+    index2++;
+}
